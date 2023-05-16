43b63c9b30b37b06b84683220bb36c07ba466031
maddy-rosets <maddysanchez@gmail.com>
2023-05-15 22:37:26 -0700

    for reals

--- Car Racing Game MOO ICT/Form1.Designer.cs
+++ Car Racing Game MOO ICT/Form1.Designer.cs
@@ -55,6 +55,7 @@
             // 
             // panel1
             // 
+            resources.ApplyResources(this.panel1, "panel1");
             this.panel1.BackColor = System.Drawing.Color.Black;
             this.panel1.Controls.Add(this.AI2);
             this.panel1.Controls.Add(this.AI1);
@@ -63,58 +64,57 @@
             this.panel1.Controls.Add(this.player);
             this.panel1.Controls.Add(this.roadTrack2);
             this.panel1.Controls.Add(this.roadTrack1);
-            resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
             // 
             // AI2
             // 
-            this.AI2.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carGrey;
             resources.ApplyResources(this.AI2, "AI2");
+            this.AI2.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carGrey;
             this.AI2.Name = "AI2";
             this.AI2.TabStop = false;
             this.AI2.Tag = "carRight";
             // 
             // AI1
             // 
-            this.AI1.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carGreen;
             resources.ApplyResources(this.AI1, "AI1");
+            this.AI1.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carGreen;
             this.AI1.Name = "AI1";
             this.AI1.TabStop = false;
             this.AI1.Tag = "carLeft";
             // 
             // award
             // 
-            this.award.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.bronze;
             resources.ApplyResources(this.award, "award");
+            this.award.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.bronze;
             this.award.Name = "award";
             this.award.TabStop = false;
             // 
             // explosion
             // 
-            this.explosion.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.explosion;
             resources.ApplyResources(this.explosion, "explosion");
+            this.explosion.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.explosion;
             this.explosion.Name = "explosion";
             this.explosion.TabStop = false;
             // 
             // player
             // 
-            this.player.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carYellow;
             resources.ApplyResources(this.player, "player");
+            this.player.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carYellow;
             this.player.Name = "player";
             this.player.TabStop = false;
             // 
             // roadTrack2
             // 
-            this.roadTrack2.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.roadTrack;
             resources.ApplyResources(this.roadTrack2, "roadTrack2");
+            this.roadTrack2.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.roadTrack;
             this.roadTrack2.Name = "roadTrack2";
             this.roadTrack2.TabStop = false;
             this.roadTrack2.Click += new System.EventHandler(this.roadTrack2_Click);
             // 
             // roadTrack1
             // 
-            this.roadTrack1.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.roadTrack;
             resources.ApplyResources(this.roadTrack1, "roadTrack1");
+            this.roadTrack1.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.roadTrack;
             this.roadTrack1.Name = "roadTrack1";
             this.roadTrack1.TabStop = false;
             // 
@@ -144,6 +144,7 @@
             // 
             // langPicker
             // 
+            resources.ApplyResources(this.langPicker, "langPicker");
             this.langPicker.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.langPicker.FormattingEnabled = true;
             this.langPicker.Items.AddRange(new object[] {
@@ -151,7 +152,6 @@
             resources.GetString("langPicker.Items1"),
             resources.GetString("langPicker.Items2"),
             resources.GetString("langPicker.Items3")});
-            resources.ApplyResources(this.langPicker, "langPicker");
             this.langPicker.Name = "langPicker";
             this.langPicker.SelectedIndexChanged += new System.EventHandler(this.langPicker_SelectedIndexChanged);
             // 
